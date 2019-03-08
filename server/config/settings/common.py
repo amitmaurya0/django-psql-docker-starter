35631ac91167fb7d749c128a1d0435f91dc39a70
archit0 <architdwivedi00@gmail.com>
2019-03-08 10:40:33 +0530

    Reading env from environment

--- server/config/settings/common.py
+++ server/config/settings/common.py
@@ -1,8 +1,9 @@
+
 import os
 import environ
 
 env = environ.Env()
-environ.Env.read_env('.env')
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
